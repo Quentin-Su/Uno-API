fa920a64a27a444829d06f6199eee0f1f47be3fd
Quentin-Su <Quentin.Sussetto@outlook.fr>
2024-01-28 21:32:20 +0100

    feat: Storing User Connections to the WebSocket

--- src/WebSocket/WebSocketServer.php
+++ src/WebSocket/WebSocketServer.php
@@ -2,23 +2,99 @@
 
 namespace App\WebSocket;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
-class WebSocketServer implements MessageComponentInterface {
-    public function onOpen(ConnectionInterface $conn) {
-        $jwt = $conn->httpRequest->getHeader('Authorization')[0] ?? null;        
+class WebSocketServer implements MessageComponentInterface
+{
+    /**
+     * @var JWTTokenManagerInterface $jwtManager
+     */
+    private $jwtManager;
+
+    /**
+     * @var EntityManagerInterface $entityManager
+     */
+    private $entityManager;
+
+    /**
+     * @var \SplObjectStorage $clients
+     */
+    protected $clients;
+
+    public function __construct(JWTTokenManagerInterface $jwtManager, EntityManagerInterface $entityManager)
+    {
+        $this->jwtManager = $jwtManager;
+        $this->entityManager = $entityManager;
+        $this->clients = new \SplObjectStorage;
     }
 
-    public function onMessage(ConnectionInterface $from, $msg) {
+    public function onOpen(ConnectionInterface $conn)
+    {
+        $jwtToken = $conn->httpRequest->getHeader('Authorization')[0] ?? null;
+
+        if ($jwtToken) {
+            try {
+                $tokenData = $this->jwtManager->parse($jwtToken);
+
+                $username = $tokenData['username'];
+                $user = $this->entityManager->getRepository('App\Entity\User')->findOneBy(['username' => $username]);
+
+                if ($user) {
+                    $this->clients->attach(new UserIdObject($user->getId()), $conn);
+
+                    dump($this->clients);
+                }
+                else {
+                    return $conn->close(1011, 'User not found');
+                }
+            } 
+            catch (\Exception $e) {
+                return $conn->close(4001, 'Invalid JWT Token');
+            };
+        } 
+        else {
+            return $conn->close(4000, 'JWT Token missing');
+        }
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg)
+    {
 
     }
 
-    public function onClose(ConnectionInterface $conn) {
+    public function onClose(ConnectionInterface $conn)
+    {
+        foreach ($this->clients as $user) {
+            if ($this->clients->offsetGet($user) === $conn) {
+                $this->clients->detach($user);
+                break;
+            }
+        }
 
+        dump($this->clients);
+        return $conn->close(1000);
     }
 
-    public function onError(ConnectionInterface $conn, \Exception $e) {
+    public function onError(ConnectionInterface $conn, \Exception $e)
+    {
+        
+    }
+}
 
+class UserIdObject 
+{
+    private $userId;
+
+    public function __construct($userId)
+    {
+        $this->userId = $userId;
+    }
+
+    public function getId()
+    {
+        return $this->userId;
     }
 }
