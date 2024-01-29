b296e8b16dba71b2406aa7a1623e251df5fd3923
Quentin-Su <Quentin.Sussetto@outlook.fr>
2024-01-29 23:49:49 +0100

    feat: GameRepository Query

--- src/Repository/GameRepository.php
+++ src/Repository/GameRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Game;
+use App\Entity\UserGame;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -21,28 +22,41 @@ class GameRepository extends ServiceEntityRepository
         parent::__construct($registry, Game::class);
     }
 
-//    /**
-//     * @return Game[] Returns an array of Game objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('g')
-//            ->andWhere('g.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('g.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    /**
+    * @return Game|null
+    */
+    public function findUserCurrentGame($userId): ?Game 
+    {
+        return $this->createQueryBuilder('g')
+            ->join('App\Entity\UserGame', 'ug', 'WITH', 'g.id = ug.game_id')
+            ->where('g.status NOT IN (:statuses)')
+            ->andWhere('ug.user_id = :userId')
+            ->andWhere('ug.status = :userGameStatus')
+            ->setParameters([
+                'statuses' => ['end', 'quit'],
+                'userId' => $userId,
+                'userGameStatus' => 'on',
+            ])
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
 
-//    public function findOneBySomeField($value): ?Game
-//    {
-//        return $this->createQueryBuilder('g')
-//            ->andWhere('g.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    /**
+    * @return array Returns an array of user IDs present in the targeted game
+    */
+    public function getUsersIds($gameId): array
+    {
+        return $this->createQueryBuilder('g')
+            ->select('DISTINCT u.id')
+            ->join('g.userGames', 'ug')
+            ->join('ug.user_id', 'u')
+            ->where('ug.status = :userGameStatus')
+            ->andWhere('g.id = :gameId')
+            ->setParameters([
+                'userGameStatus' => 'on',
+                'gameId' => $gameId,
+            ])
+            ->getQuery()
+            ->getResult();
+    }
 }
